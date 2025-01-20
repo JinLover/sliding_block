ce52da3c96efe4d726ea3d4bf74ae2504461ec8e
JinLover <51523331+JinLover@users.noreply.github.com>
2025-01-19 21:57:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+class block_puzzle():
+    def __init__(self, n):
+        self.map = [[(i+n*j)%(n**2) for i in range(1, n+1)] for j in range(n)]
+        self.answer = [[(i+n*j)%(n**2) for i in range(1, n+1)] for j in range(n)]
+        self.blank = [n-1, n-1]
+        
+    def adjacent(self, co1, co2):
+        y1, x1 = co1
+        y2, x2 = co2
+        dis = ((x1-x2)**2 + (y1-y2)**2)**0.5
+        if dis == 1:
+            return True
+        return False
+        
+    def move(self, position):
+        position -= 1
+        # check tile position is valid
+        assert 0 <= position and position <= n**2-1
+        coord = [position // n, position % n]
+        # check tile is adjacent to blank
+        assert self.adjacent(self.blank, coord)
+        self.map[coord[0]][coord[1]], self.map[self.blank[0]][self.blank[1]] = self.map[self.blank[0]][self.blank[1]], self.map[coord[0]][coord[1]]
+        self.blank = coord
+        
+    def check(self):
+        if self.map == self.answer:
+            return True
+        return False
+
+block = block_puzzle(3)
+print(block.map)
+block.move(6)
+print(block.map)
+block.check()
