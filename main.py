c9518977f410527501cdfd74d04ae6faa139c58d
JinLover <51523331+JinLover@users.noreply.github.com>
2025-01-19 23:42:28 -0500

    rollback distance_answer, remove test function and implement heuristic BFS

--- main.py
+++ main.py
@@ -37,6 +37,16 @@ class Block():
         assert self.adjacent(self.blank, coord)
         self.map[coord[0]][coord[1]], self.map[self.blank[0]][self.blank[1]] = self.map[self.blank[0]][self.blank[1]], self.map[coord[0]][coord[1]]
         self.blank = coord
+        
+    def distance_answer(self):
+        distance = 0
+        for i in range(self.n):
+            for j in range(self.n):
+                num = self.map[i][j] - 1
+                if num == -1:
+                    continue
+                distance += abs(num // self.n - i) + abs(num % self.n - j)
+        return distance
     
     def check(self) -> bool:
         if self.map == self.answer:
@@ -62,38 +72,8 @@ class Block():
             order.append(available[randint])
         return order
     
-
 if __name__ == "__main__":
-    import random
-    
-    def test(map, blank: list[int], target: int | list[int]) -> list[list[int]]:
-        n = len(map)
-        if isinstance(target, int):
-            position = target - 1
-        elif isinstance(target, list):
-            position = n * target[0] + target[1]
-
-        # check tile position is valid
-        assert 0 <= position < n**2
-        coord = [position // n, position % n]
-        
-        # [:] make new object!
-        result = [row[:] for row in map]
-        # result = [row.copy() for row in map]
-        result[coord[0]][coord[1]], result[blank[0]][blank[1]] = result[blank[0]][blank[1]], result[coord[0]][coord[1]]
-        return result, coord
-    
-    def distance_answer(map):
-        n = len(map)
-        distance = 0
-        for i in range(n):
-            for j in range(n):
-                num = map[i][j] - 1
-                if num == -1:
-                    distance += abs(n - 1 - i) + abs(n - 1 - j)
-                else:
-                    distance += abs(num // n - i) + abs(num % n - j)
-        return distance
+    import random, copy
     
     block = Block(3)
     block.map = [[1,3,6],[4,0,2],[7,5,8]]
@@ -107,14 +87,16 @@ if __name__ == "__main__":
     # block.print("answer")
     avail_count = len(block.available())
     queue = list(zip(block.available()[:], 
-                     [block.map[:][:] for _ in range(avail_count)], 
-                     [block.blank[:] for _ in range(avail_count)],
-                     [distance_answer(block.map) for _ in range(avail_count)]))
+                     [copy.deepcopy(block) for _ in range(avail_count)]))
     while queue:
-        tile, map, blank, score = queue.pop(0)
-        new_map, new_blank = test(map, blank, tile)
-        new_score = distance_answer(new_map)
+        tile, object = queue.pop(0)
+        score = object.distance_answer()
+        object.move(tile)
+        new_score = object.distance_answer()
+        if object.check():
+            break
         if new_score < score:
-            queue.append()
-    
+            for tile in object.available():
+                queue.append((tile, copy.deepcopy(object)))
+    object.print()
     # print(block.distance_answer())
